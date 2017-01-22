5eb512a966b8e261e3723abb0dc88b131b73662b
Kenny Pyatt <kenny.pyat@gmail.com>
2017-01-22 10:55:26 -0600

    Added LDAP users to sudoers file

--- python-ldap/load_db.py
+++ python-ldap/load_db.py
@@ -3,6 +3,7 @@
 from tinydb import TinyDB, Query, where
 import socket
 import sys
+import re
 
 db = TinyDB('db.json')   # The database we are storing this in
 
@@ -12,7 +13,8 @@ db.purge()
 for ip in range(51,90):
    ip_addr = '192.168.1.' + str(ip)
    host_info = socket.gethostbyaddr(ip_addr)
-   db.insert({'ip': ip_addr, 'name': host_info[0], 'username':'', 'password':'', 'firstname':'', 'lastname': ''})
+   if not re.search('odroid', host_info[0]):
+      db.insert({'ip': ip_addr, 'name': host_info[0], 'username':'', 'password':'', 'firstname':'', 'lastname': ''})
 
 # This will find a free VM
 #vms = Query()
