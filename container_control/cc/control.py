ecff4a2d07312948a919413e32e7a6558830c6d2
Kenny Pyatt <kenny.pyatt@gmail.com>
2018-07-29 17:13:00 +0000

    Added create container to container control

--- container_control/cc/control.py
+++ container_control/cc/control.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from cc import app
 import docker
+import os
 
 
 # List of containers ==========================================================
@@ -71,4 +72,21 @@ def stop_container(vm_name):
 
 # Create Container ============================================================
 # This one isn't as simple since I need to do a bunch of things so I created
-# a bash script and just run it
+# a bash script and just run it.  It creates keys, sets up OpenVPN, makes
+# a Docker image (with Dockerfile) and then runs the container
+@app.route('/1.0/containers/create')
+def create_container():
+
+    response = {}
+    response['success'] = 'false'
+
+    vm_name = request.args.get('vm_name')
+    ip_addr = request.args.get('ip')
+
+    print(vm_name, ip_addr)
+    #create_dev_container.sh
+    create_vm = os.popen("/root/create_dev_containers.sh " + vm_name + " " + ip_addr).readlines()
+    if create_vm:
+        response['success'] = 'true'
+
+    return jsonify(response)
